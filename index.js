49569fcbadfd53215aa62003393b95de81f02223
Alessandro Muraro <akmuraro@gmail.com>
2019-12-22 01:06:15 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MyComponent extends Component {
+  state = {
+    something: 'test'
+  }
+
+  render() {
+    return <div>Test</div>
+  }
+}
+
+export default MyComponent
